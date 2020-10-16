7cdcf0eb1ed649cf1be8abfa156148c72840978d
Logan Rothrock <loganrothrock@outlook.com>
2020-10-16 15:21:42 -0500

    Data Layer

--- StoreFront.DATA.EF/Metadata/DBStoreFrontMetadata.cs
+++ StoreFront.DATA.EF/Metadata/DBStoreFrontMetadata.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StoreFront.DATA.EF.Metadata
+namespace StoreFront.DATA.EF
 {
-    class DBStoreFrontMetadata
+    [MetadataType(typeof(StoreFrontMetadata))]
+    public partial class StoreFront { }
+
+    public class StoreFrontMetadata
     {
+        
     }
 }
