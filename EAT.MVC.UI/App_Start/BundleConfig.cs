b23fd6d3dd7fd216ddba040b0deaf5d63f20b86c
EliNeedham <EliNeedham@outlook.com>
2020-11-13 10:53:48 -0600

    updated the hrefs for scheduling and admin drop down bars, added usersadmin and rolesadmin to navbar for admin,

--- EAT.MVC.UI/App_Start/BundleConfig.cs
+++ EAT.MVC.UI/App_Start/BundleConfig.cs
@@ -8,7 +8,20 @@ namespace EAT.MVC.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Content/js/jquery.min.js",
+                        "~/Content/js/jquery.easing.1.3.js",
+                        "~/Content/js/bootstrap.min.js",
+                        "~/Content/js/jquery.waypoints.min.js",
+                        "~/Content/js/jquery.stellar.min.js",
+                        "~/Content/js/owl.carousel.min.js",
+                        "~/Content/js/jquery.flexslider-min.js",
+                        "~/Content/js/jquery.countTo.js",
+                        "~/Content/js/jquery.magnific-popup.min.js",
+                        "~/Content/js/magnific-popup-options.js",
+                        "~/Content/js/simplyCountdown.js",
+                        "~/Content/js/main.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +37,17 @@ namespace EAT.MVC.UI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/animate.css",
+                      "~/Content/css/icomoon.css",
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/magnific-popup.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/owl.theme.default.min.css",
+                      "~/Content/css/flexslider.css",
+                      "~/Content/css/pricing.css",
+                      "~/Content/css/style.css"
+                      ));
         }
     }
 }
